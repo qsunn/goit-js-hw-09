1e323dbd4954e98177d72b5b5bb939cd41ca806d
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-09-04 18:34:06 +0300

    task_01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,6 @@
 const start = document.querySelector('[data-start]');
-const end = document.querySelector('[data-end]');
+const stop = document.querySelector('[data-stop]');
+let interval;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -7,10 +8,10 @@ function getRandomHexColor() {
 
 start.addEventListener('click', () => {
     start.disabled = true;
-    const interval = setInterval(() => document.body.style.background = getRandomHexColor(), 1000);
+    interval = setInterval(() => document.body.style.background = getRandomHexColor(), 1000);
 })
 
-end.addEventListener('click', () => {
+stop.addEventListener('click', () => {
     start.disabled = false;
     clearInterval(interval);
 })
