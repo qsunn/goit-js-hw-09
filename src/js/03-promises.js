cd9b4db5da43815c6bf2dad9250667166fb987e6
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-09-07 13:29:18 +0300

    upg

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,25 +3,20 @@ const form = document.querySelector('.form');
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      setTimeout(() => resolve({ position, delay }), delay);
-    } else {
-      setTimeout(() => reject({ position, delay }), delay);
-    };
-  }).then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  }).catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+    setTimeout(() => shouldResolve ? resolve({ position, delay }) : reject({ position, delay }), delay);
+  }).then(({ position, delay }) => console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
+    .catch(({ position, delay }) => console.log(`❌ Rejected promise ${position} in ${delay}ms`));
 };
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+
   const amount = +form.elements.amount.value;
   const step = +form.elements.step.value;
   let delay = +form.elements.delay.value;
+  
   for (let i = 1; i <= amount; i++) {
     createPromise(i, delay);
-    delay +=step;
+    delay += step;
   };
 });
